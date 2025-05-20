d0f8aeeb7b5a5ff0b088a9c0d41a70173676b288
user150434 <o.boudaati@gmail.com>
2025-05-20 11:45:31 +0200

    yup

---
+++ eersteproject/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+int leeftijd = 19;
+string naam = "mario";
+bool isKlein = true;
+float lengte = 1.85f;
+console.WriteLine("Hallo " + naam + ", je bent " + leeftijd + " jaar oud.");
